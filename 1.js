00298976aa5e37eaf8bef9c44ca4945c6a2b8172
Tetiana Humeniuk <tetiana_humeniuk@epam.com>
2022-01-23 22:19:09 +0200

    1 task done

--- 1.js
+++ 1.js
@@ -1,1 +1,12 @@
-new
+console.log('foo' + false) // string + boolean ->
+console.log("agent" + 7) //string+number ->
+console.log(7 + false) // number+boolean  ->
+console.log("name" * true) // string * boolean
+console.log("cat" * 1) // string * number,
+console.log(1 * false) // number * boolean
+console.log("dog" / true) // string/boolean
+console.log("dog" / 9) // string/number
+console.log(4 / true)  // number/Boolean
+console.log(String(1))
+console.log(Number(null))// number, string, boolean
+console.log(Boolean(1))
